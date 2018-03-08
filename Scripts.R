44d6c8ee29304026d78d165f08bcb273a8f6ce44
Philip Hossu <phossu@hawk.iit.edu>
2018-03-08 15:43:10 -0600

    Added data, test data, began function to read file

--- Scripts.R
+++ Scripts.R
@@ -1,1 +1,21 @@
-#:D
+# Problem 1
+# Data Source: https://files.oakland.edu/users/grossman/enp/Erdos1.html
+
+processFile = function(filepath) {
+  con = file(filepath, "r")
+  while (TRUE) {
+    line = readLines(con, n = 1)
+    if (length(line) == 0) {
+      break
+    }
+    # Our code goes here
+    print(line)
+  }
+  close(con)
+}
+
+processFile("~/Desktop/School/MATH380/Modeling-With-Networks/erdos1_testdata.csv")
+
+# Problem 2
+
+# Problem 3
