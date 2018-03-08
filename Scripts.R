6919e2b4857b6a543e21971d47c632339331d334
Philip Hossu <phossu@hawk.iit.edu>
2018-03-08 16:43:11 -0600

    Worked on erdos 1 file parsing code

--- Scripts.R
+++ Scripts.R
@@ -1,20 +1,51 @@
 # Problem 1
 # Data Source: https://files.oakland.edu/users/grossman/enp/Erdos1.html
 
+# 511 x 1 column of objects where each object has first attribute of name, second attribute as a list of collaborators
+df <- data.frame()
+currentMain <- ""
+
 processFile = function(filepath) {
   con = file(filepath, "r")
+  
+  line = readLines(con, n = 1)
+  words <- strsplit(line, " ")[[1]]
+  temp <- gsub('[0-9:*]+', '', words)
+  currentMain <- paste(words <- temp[which(temp != '')], collapse = ' ')
+  
   while (TRUE) {
     line = readLines(con, n = 1)
+    print(line)
     if (length(line) == 0) {
       break
     }
-    # Our code goes here
-    print(line)
+    if (line == ''){
+      line = readLines(con, n = 1)
+      words <- strsplit(line, " ")[[1]]
+      temp <- gsub('[0-9:*]+', '', words)
+      currentMain <- paste(words <- temp[which(temp != '')], collapse = ' ')
+    }
+    else{
+      words <- strsplit(line, " ")[[1]]
+      temp <- gsub('[*]', '', words)
+      words <- paste(words <- temp[which(temp != '')], collapse = ' ')
+      currentDF <- data.frame(currentMain,words)
+      df <- rbind(df,currentDF)
+    }
+    if (line == "&"){
+      break
+    }
+    #print(currentMain)
+
+    # Removing spaces, year, extra symbols from main lines
   }
   close(con)
 }
 
 processFile("~/Desktop/School/MATH380/Modeling-With-Networks/erdos1_testdata.csv")
+
+colnames(df) <- c("Main/From", "Secondary/To")
+
 
 # Problem 2
 
