c4afd6c9d9e7c64a3e7401ff3dcdf2a22d015143
Paolo Ratti Tamayo <prattita@hawk.iit.edu>
2018-04-13 03:18:19 -0500

    Calculated alpha centrality for the citation network and plotted the results

--- ScriptsCitations.R
+++ ScriptsCitations.R
@@ -20,4 +20,13 @@ plot(citationNetwork, edge.arrow.size=.01,vertex.size=1,margin=0, vertex.label=N
 zm()
 
 #Alpha centrality
-alpha_centrality(citationNetwork, nodes = V(citationNetwork), alpha = 1, loops = FALSE, exo = 1)
+citationAlpha <- alpha_centrality(citationNetwork, nodes = V(citationNetwork), alpha = 0.9, loops = FALSE, exo = 1)
+
+sort(citationAlpha)
+
+pal <- colorRampPalette(c("blue","green","yellow","orange","red"))
+graphCol <- pal(5000)[as.numeric(cut(citationAlpha,breaks=5000))]
+
+plot(citationNetwork, vertex.color=graphCol, edge.arrow.size=.001, edge.size=NA,vertex.label=NA,vertex.size=1,margin=0)
+zm()
+
